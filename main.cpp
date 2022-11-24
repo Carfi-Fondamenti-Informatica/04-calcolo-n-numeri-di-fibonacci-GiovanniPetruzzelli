09c10f55fab4c230769b9ecd3da55dc1bdf1d686
GiovanniPetruzzelli <117984097+GiovanniPetruzzelli@users.noreply.github.com>
2022-11-24 21:11:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 int main() {
-    int n=0,a=1,b=1,c=0;
+   int n=0,a=1,b=1,c=0;
     cin>>n;
     if(n>=2) {
-        cout<<a<<endl<<b<<endl;
-        for(int i=2; i<n;i++) {
-            c=a+b;
-            a=b;
-            b=c;
-            cout<<c<<endl;
+        cout << a << endl << b << endl;
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            cout << c << endl;
         }
-   else {
+    }
+    else  {
         cout<<"errore"<<endl;
-   }
+
+        }
 
    return 0;
 }
